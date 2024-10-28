4fab36a92782febb7ee86b6b352eafc11d6ffe05
AUniverse <avoteamoficial@Gmail.com>
2024-10-28 12:43:59 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+model = PPO.load("drone_rl_model")
+
+obs = env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
